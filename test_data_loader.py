97a8c7804a91966980092a1031288f6d227eb53c
Yuvraj Rajpurohit <yuvrajrajpurohitt@gmail.com>
2025-12-18 10:51:16 +0530

    Delete test_data_loader.py

--- test_data_loader.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from data_loader import load_data
-
-def test_invalid_file_type():
-    try:
-        load_data("data.txt")
-    except ValueError as e:
-        assert "CSV" in str(e)
